f7ded7304955dc234f1df99b03799b86476c1445
Leandro Bevilacqua <leabevi@gmail.com>
2023-06-01 19:51:37 -0300

    agregados los datos  requeridos en los componentes Header, Informacion y Formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -11,9 +11,19 @@ const Formulario = () => {
 
   return (
     <form onSubmit={handleSubmit}>
+      <h5>Nombre</h5>
       <input type="text" value={campo1} onChange={(e) => setCampo1(e.target.value)} />
+      <h5>Apellido</h5>
       <input type="text" value={campo2} onChange={(e) => setCampo2(e.target.value)} />
-      <button type="submit">Enviar</button>
+      <h5>Correo electronico del trabajo</h5>
+      <input type="text" value={campo1} onChange={(e) => setCampo1(e.target.value)} />
+      <h5>Pais</h5>
+      <input type="text" value={campo2} onChange={(e) => setCampo2(e.target.value)} />
+      <h5>Numero de telefono</h5>
+      <input type="text" value={campo1} onChange={(e) => setCampo1(e.target.value)} />
+      <h5>Puesto de trabajo</h5>
+      <input type="text" value={campo2} onChange={(e) => setCampo2(e.target.value)} />
+      <button type="submit">Inscribite</button>
     </form>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
 function Header() {
     return (
       <header>
-        <h1>Encabezado de la aplicación</h1>
+        <h6>WEBINAR</h6>
+        <h1>El reto de humanizar el CX financiero en 2021.</h1>
+        <h1>La experiencia de un Unicornio de Latam</h1>
+        <h2>MIERCOLES 16 DE DICIEMBRE | 17 HS (HORARIO DE QUITO)</h2>
       </header>
     );
   }
